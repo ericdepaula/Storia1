42d1c3c3daf7930aa4c5aff8a43e1e7f71351739
Eric de Paula <ericdepaula39@gmail.com>
2025-09-04 08:14:01 -0300

    Refactor adiciona preco para fluxo de pagamento em centavos

--- storia-api/src/config/stripeConfig.js
+++ storia-api/src/config/stripeConfig.js
@@ -7,23 +7,27 @@ export const planosDeProduto = {
     nome: "Agenda Estratégica de 5 Dias",
     dias: 5,
     isFree: true,
+    precoEmCentavos: 0,
   },
   // Cole aqui o ID do Preço do seu plano de 30 dias
   price_1RkvTvPphAIQfHkyLv2HNYci: {
     nome: "Agenda Estratégica de 30 Dias",
     dias: 30,
     isFree: false,
+    precoEmCentavos: 2990,
   },
   // Cole aqui o ID do Preço do seu plano de 60 dias
   price_1RlVzHPphAIQfHkypaLBoAxR: {
     nome: "Agenda Estratégica de 60 Dias",
     dias: 60,
     isFree: false,
+    precoEmCentavos: 4990,
   },
   // Cole aqui o ID do Preço do seu plano de 90 dias
   price_1RlW0CPphAIQfHkyzHVlqqyx: {
     nome: "Agenda Estratégica de 90 Dias",
     dias: 90,
     isFree: false,
+    precoEmCentavos: 9990,
   },
 };
