0820fd4cd0cab6c6a727dc9e635370eb56b41068
Eric de Paula <ericdepaula39@gmail.com>
2025-09-08 12:38:56 -0300

    Fix(pagamento): Corrigi insert DB

--- storia-api/src/services/pagamentoService.js
+++ storia-api/src/services/pagamentoService.js
@@ -86,7 +86,7 @@ const criarCobrancaPix = async (priceId, promptData, usuarioId, taxId) => {
     const { error: insertError } = await supabase.from("compras").insert({
       usuario_id: usuarioId,
       payment_session_id: abacateData.id,
-      produto_id: abacateData.products.id,
+      produto_id: abacateData.products[0].id,
       preco_id: priceId,
       valor_total: plano.precoEmCentavos / 100,
       status_pagamento: "PENDENTE",
